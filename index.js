38bde0d0cbe1933170bd01d354255bbfa1f278a8
roy <kimathiwaweru@gmail.com>
2023-03-15 12:44:19 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 function scuberGreetingForFeet(ride){
+  let message;
   if (ride <= 400){
-    return "This one is on me!";
-  }else if(ride > 2000){
-    return "I will gladly take your thirty bucks.";
-  }else if (ride>=2500)   
-  {return "No can do."  ;}    
-
+    message = "This one is on me!"
+    return message;
+  }else if(ride > 2000 && ride <=2500){
+    message = "I will gladly take your thirty bucks.";
+    return message;
+  }else{
+    message = "No can do.";
+    return message;
+  }    
 }
 function ternaryCheckCity(city){
-  city === 'NYC' ? "Ok, sounds good." : "No go." ; 
+  let message;
+  city === 'NYC' ? (message = "Ok, sounds good.") : (message = "No go.") ;
+  return message; 
 }
 
 function switchOnCharmFromTip(tip){
-  switch (generous){
-    case true :
-       tip = "Thank you so much."; 
-      break;
-    
-    case false :
-       tip = "Thank you";
+  let message;
+  switch (tip){
+    case "generous" :
+       message = "Thank you so much.";      
+      break;    
+    case "not as generous" :
+       message = "Thank you.";
       break;
     default :
-     tip = "Bye";
-    break;
+     message = "Bye.";
+     break;     
   }
-  return tip;
+  return message;
 }
 
 
